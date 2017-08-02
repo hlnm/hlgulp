3fe9f9b525a5c017edc6c539179ebaa36846cc37
hlnm <hualuxinmei@outlook.com>
2017-08-02 18:33:47 +0800

    添加css自动浏览器前缀

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,6 @@ const pngquant = require('imagemin-pngquant');
 const config = require('./gulp-config/config');
 
 
-gulp.task('mincss', () => {
-    gulp.src('src/css/*.css')
-        .pipe(p.minifyCss())
-        .pipe(p.base64({
-            maxImageSize: 10240, 
-            debug: true
-        }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(p.plumber({ errorHandler: handleError }))
-
-
-});
-
 // gulp.task('clean', () => {
 //     gulp.src(config.clean)
 //         .pipe(clean())
@@ -34,48 +21,69 @@ gulp.task('mincss', () => {
 
 gulp.task('imgmin', () => {
     gulp.src(config.img.src)
-        .pipe(p.cache(p.imagemin({
+
+        .pipe(p.imagemin({
             progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
+            svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
-        })))
+        }))
         .pipe(gulp.dest(config.img.dist))
-        .pipe(p.plumber({ errorHandler: handleError }))
-})
+        .pipe(p.plumber({errorHandler: handleError}))
+});
 
 gulp.task('js', () => {
     gulp.src(config.js.src)
-        .pipe(p.uglify())
+
         .pipe(gulp.dest(config.js.dist))
-        .pipe(p.plumber({ errorHandler: handleError }))
-})
+        .pipe(p.plumber({errorHandler: handleError}))
+});
 
 gulp.task('sass', () => {
     gulp.src(config.sass.src)
-        .pipe(p.sass({ outputStyle: 'compressed' }).on('error', p.sass.logError))
         .pipe(p.base64({
-            maxImageSize: 10240, 
+            extensions: [ 'png','svg', 'jpg'],
+            maxImageSize: 1024*20
+        }))
+        .pipe(p.sass().on('error', p.sass.logError))
+        .pipe(gulp.dest('dist/css-scss/'))
+        .pipe(p.plumber({errorHandler: handleError}))
+});
+gulp.task('mincss', ['sass'], () => {
+    gulp.src('dist/css-scss/*.css')
+        .pipe(p.autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(p.base64({
+
+            extensions: ['svg', 'png', 'jpg'],
+            maxImageSize: 10240,
             debug: true
         }))
-        .pipe(gulp.dest(config.sass.dist))
-        .pipe(p.plumber({ errorHandler: handleError }))
-})
+        .pipe(p.minifyCss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(p.plumber({errorHandler: handleError}))
+
+});
 
 gulp.task('jade2html', () => {
     gulp.src(config.jade.src)
-        .pipe(p.jade({
+
+        .pipe(p.cache(p.jade({
             // client: true,
             pretty: true
-        }))
+        })))
         .pipe(p.base64({
-            maxImageSize: 10240, 
+            baseDir: 'images',
+            extensions: ['svg', 'png', 'jpg'],
+            maxImageSize: 10240,
             debug: true
         }))
         .pipe(gulp.dest(config.jade.dist))
-        .pipe(p.plumber({ errorHandler: handleError }))
-})
+        .pipe(p.plumber({errorHandler: handleError}))
+});
 
-gulp.task('default', ['mincss', 'js', 'sass','jade2html', 'imgmin']);
+gulp.task('default', ['js', 'jade2html', 'imgmin', 'mincss']);
 // gulp.task('cleans',['clean'],function(){
 //     gulp.start('mincss','imgmin','js','sass')
 
@@ -84,7 +92,6 @@ gulp.task('default', ['mincss', 'js', 'sass','jade2html', 'imgmin']);
 //     gulp.start('mincss','imgmin','js')
 
 // });
-
 
 
 gulp.task('deploy', ['mincss', 'imgmin']);
